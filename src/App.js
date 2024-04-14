6f5cf7e2899379de8585804a50119bf773dd0b0d
Himanshu Maurya <mauryahimanshu567@gmail.com>
2024-04-15 01:35:49 +0530

    adding loading animations

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import React from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import {Outlet} from 'react-router-dom'
-
+import { useState,useEffect } from 'react';
+import Loading from './components/Loading/Loading'
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(()=>{
+      setIsLoading(true)
+      setTimeout(() => {
+          setIsLoading(false)
+      }, 5000);
+  },[])
+
   return (
     <>
-    <Header/>
-    <Outlet/>
-    <Footer/>
+     {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <Outlet />
+          <Footer />
+        </>
+      )}
     </>
       
       )
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+      <img src='./loading.gif' className='mx-auto mt-56' alt='loading_animation'/>
+    </div>
+  )
+}
+
+export default Loading
